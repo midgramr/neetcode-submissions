1e04f1d6459dcb0d777fcfb725a0d3c74b2b8dad
Kevin Zhang <88405328+midgramr@users.noreply.github.com>
2026-09-06 12:34:54 -0600

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> cnt(26);
+        for (auto &c : s) {
+            cnt[c - 'a']++;
+        }
+
+        for (auto &c : t) {
+            cnt[c - 'a']--;
+        }
+
+        for (int i = 0; i < 26; ++i) {
+            if (cnt[i] != 0) return false;
+        }
+        return true;
+    }
+};

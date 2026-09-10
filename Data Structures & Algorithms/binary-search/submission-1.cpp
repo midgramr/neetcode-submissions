107fea619bc3a8b011cae2f86c1c92e3cd5c9b91
Kevin Zhang <88405328+midgramr@users.noreply.github.com>
2026-09-09 20:30:33 -0600

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.cpp
@@ -0,0 +1,18 @@
+#define sz(a) (int)(a).size()
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = sz(nums) - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target) {
+                return m;
+            } else if (nums[m] < target) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return -1;
+    }
+};

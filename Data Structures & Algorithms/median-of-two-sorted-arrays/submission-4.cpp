86c0a6ca4f32013033bb7779452154f784395071
Kevin Zhang <88405328+midgramr@users.noreply.github.com>
2026-09-12 10:40:23 -0600

    Add: median-of-two-sorted-arrays - submission-4

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-4.cpp
@@ -0,0 +1,34 @@
+#define sz(a) (int)(a).size()
+using ll = long long;
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n = sz(nums1), m = sz(nums2);
+        if (n > m) return findMedianSortedArrays(nums2, nums1);
+        int total = n + m;
+        int target = (total + 1) / 2;
+        int l = 0, r = n;
+        while (l <= r) {
+            int i = l + (r - l) / 2;
+            int j = target - i;
+
+            ll al = i > 0 ? nums1[i - 1] : INT_MIN;
+            ll ar = i < n ? nums1[i] : INT_MAX;
+            ll bl = j > 0 ? nums2[j - 1] : INT_MIN;
+            ll br = j < m ? nums2[j] : INT_MAX;
+
+            if (al <= br && bl <= ar) {
+                if (total % 2 == 0) {
+                    return (max(al, bl) + min(ar, br)) / 2.0;
+                } else {
+                    return max(al, bl);
+                }
+            } else if (al > br) {
+                r = i - 1;
+            } else {
+                l = i + 1;
+            }
+        }
+        return -1;
+    }
+};
